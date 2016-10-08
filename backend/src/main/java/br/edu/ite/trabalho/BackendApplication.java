a78fd9da948f85e9085964e38babc71bd72ccc87
Aluno <horacio.dias92@gmail.com>
2016-10-07 21:18:41 -0300

    Adicionando autenticação

--- backend/src/main/java/br/edu/ite/trabalho/BackendApplication.java
+++ backend/src/main/java/br/edu/ite/trabalho/BackendApplication.java
@@ -2,6 +2,10 @@ package br.edu.ite.trabalho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,4 +13,16 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+	
 }
