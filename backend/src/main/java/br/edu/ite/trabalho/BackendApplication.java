d9bedc0d75e0f75e8035ac571d92873dade94a0d
Horácio Dias <horacio.baptista@supportweb.com>
2016-10-10 11:27:28 -0300

    Adicionando persistence.xml e possibilidade de conectar em banco diferente

--- backend/src/main/java/br/edu/ite/trabalho/BackendApplication.java
+++ backend/src/main/java/br/edu/ite/trabalho/BackendApplication.java
@@ -1,13 +1,18 @@
 package br.edu.ite.trabalho;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableJpaRepositories("br.edu.ite")
 public class BackendApplication {
 
 	public static void main(String[] args) {
@@ -24,5 +29,11 @@ public class BackendApplication {
             }
         };
     }
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistencia");
+		return entityManagerFactory;
+	}
 	
 }
