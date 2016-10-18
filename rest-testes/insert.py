aa044eacad0ca2eefc234d1b185c6b0de5561513
Horácio Dias <horacio.baptista@supportweb.com>
2016-10-18 10:01:31 -0200

    Atualizando e criando arquivo para inserir 100 clientes para testar a paginacao

---
+++ rest-testes/insert.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+import json
+import requests as r
+
+cabecalho = {'Accept':'application/json','Content-Type':'application/json'}
+
+requisicoes_okay = 0
+
+for i in range(100):
+    dados = {
+        "nome":"Jose %i"%(i),
+        "email":"josedasilva@gmail.com%i"%(i),
+        "rg": 123456789,
+        "telefone":"+55 14 9 1234-5%03d"%(i),
+        "endereco":{
+            "rua":"Rua dos Bobos%i"%(i),
+            "numero":"%i"%(i),
+            "bairro":"Nenhum%i"%(i),
+            "cidade":"Wherever%i"%(i)
+        }
+    }
+
+    resp = r.post('http://admin:admin123@localhost:8000/cliente/', headers=cabecalho, data=json.dumps(dados, encoding='utf-8') )
+    okay = resp.status_code == 200 or resp.status_code == 201
+    if okay:
+        requisicoes_okay += 1
+
+print(requisicoes_okay)
